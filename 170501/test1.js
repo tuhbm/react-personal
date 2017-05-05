334b0f9071ae1d0e09c8a3c49a25886c106d9e71
김태균 <ktk@gimtaegyun-ui-MacBook-Pro.local>
2017-05-05 15:52:59 +0900

    first

---
+++ 170501/test1.js
@@ -0,0 +1,18 @@
+class Codelab extends React.Component{
+    render(){
+        return(
+            <div>Codelab</div>
+        );
+    }
+}
+
+class App extends React.Component{
+    render(){
+        return(
+            <Codelab/>
+    );
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+
